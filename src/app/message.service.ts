6ec64e8c19b1657a324045b450cc3ffe7510913d
cyb <1587443838@qq.com>
2019-01-01 20:18:03 +0800

    修改注释

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 @Injectable(/*{
-  providedIn: 'root',//放出来会报错 ERROR in src/app/message.service.ts(2,2): error TS2554: Expected 0 arguments, but got 1.
+  providedIn: 'root',//放出来这个会报错 ERROR in src/app/message.service.ts(2,2): error TS2554: Expected 0 arguments, but got 1.
 
 
  }*/)
